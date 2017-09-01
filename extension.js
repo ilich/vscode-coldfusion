d9c9a7920416cb1720ff4d875325ba3efd034e84
Eric <eric@ericyd.com>
2017-09-01 07:23:27 -0700

    Update regular expressions to accurately determine code context

--- extension.js
+++ extension.js
@@ -1,20 +1,20 @@
-// https://github.com/trstt/cfml-comment-tags
-
-// or, could just try to merge with This
-// https://github.com/ilich/vscode-coldfusion
-
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+/**
+ * Determine code context on ColdFusion files to determine whether to use
+ * tag-style comments or cfscript-style comments.
+ * 
+ * Uses a simple regex to determine if cf tags are present, and if so
+ * whether the selection is within a cfscript tag.
+ */
 function activate(context) {
   let disposable = vscode.commands.registerCommand(
     'extension.cfml-comment-tags',
     function() {
-      // The code you place here will be executed every time your command is executed
       var editor = vscode.window.activeTextEditor;
 
-      // set cfscript config by default
+      // define cfscript config
+      // taken from ./cfscript.configuration.json
       let languageConfig = {
         comments: {
           lineComment: '//',
@@ -22,37 +22,13 @@ function activate(context) {
         },
         brackets: [['{', '}'], ['[', ']'], ['(', ')']],
         autoClosingPairs: [
-          {
-            open: '{',
-            close: '}'
-          },
-          {
-            open: '[',
-            close: ']'
-          },
-          {
-            open: '(',
-            close: ')'
-          },
-          {
-            open: '#',
-            close: '#'
-          },
-          {
-            open: "'",
-            close: "'",
-            notIn: ['string', 'comment']
-          },
-          {
-            open: '"',
-            close: '"',
-            notIn: ['string']
-          },
-          {
-            open: '/**',
-            close: ' */',
-            notIn: ['string']
-          }
+          { open: '{', close: '}' },
+          { open: '[', close: ']' },
+          { open: '(', close: ')' },
+          { open: '#', close: '#' },
+          { open: "'", close: "'", notIn: ['string', 'comment'] },
+          { open: '"', close: '"', notIn: ['string'] },
+          { open: '/**', close: ' */', notIn: ['string'] }
         ],
         surroundingPairs: [
           ['{', '}'],
@@ -64,60 +40,45 @@ function activate(context) {
         ]
       };
 
-      try {
-        if (editor != undefined) {
-          const selection = editor.selection;
-          const textBeforeSelection = editor.document.getText(
-            new vscode.Range(new vscode.Position(0, 0), selection.start)
-          );
-          // if no <cf> tags, assume cfscript
-          // console.log(
-          //   textBeforeSelection
-          //   .replace(editor.document.eol === 1 ? /\n/gm : /\n\r/gm, '')
-          //   .match(/\<cf(?!.*\<cf).{6}/g)[0],
-          //   textBeforeSelection
-          //   .replace(editor.document.eol === 1 ? /\n/gm : /\n\r/gm, '')
-          //   .search(/\<\/cfscript/g)
-          //   // ,textBeforeSelection.replace(editor.document.eol == 1 ? /\n/ : /\n\r/, ''),
-          //   // editor.document.eol
-          // );
-          if (
-            // <cf tag exists
-            textBeforeSelection.search(/\<cf/) !== -1 &&
-            // the last instance is not <cfscript
-            (textBeforeSelection
-              .replace(editor.document.eol === 1 ? /\n/gm : /\n\r/gm, '')
-              .match(/\<cf(?!.*\<cf).{6}/g)[0] !== '<cfscript' ||
-              // there is a cfscript tag above you
-              textBeforeSelection
-                .replace(editor.document.eol === 1 ? /\n/gm : /\n\r/gm, '')
-                .search(/\<\/cfscript/g) !== -1 ||
-              // selection contains "<cfscript> or </cfscript>"
-              editor.document
-                .getText(editor.selection.line)
-                .search(/<\/?cfscript/) !== -1)
-          ) {
-            languageConfig = Object.assign(languageConfig, {
-              comments: {
-                blockComment: ['<!---', '--->']
-              }
-            });
-          }
-          // set language config, which should enable the right kind of comments
-          vscode.languages.setLanguageConfiguration(
-            'lang-cfml',
-            languageConfig
-          );
-          vscode.commands.executeCommand('editor.action.commentLine');
+      if (editor != undefined) {
+        const selection = editor.selection;
+        const textBeforeSelection = editor.document.getText(
+          new vscode.Range(new vscode.Position(0, 0), selection.start)
+        );
+        const fullSelectionText = editor.document.getText(
+          new vscode.Range(
+            new vscode.Position(editor.selection.start.line, 0),
+            editor.document.lineAt(editor.selection.end).range.end
+          )
+        );
+
+        // if no <cf> tags, assume cfscript
+        // if text is within a <cfscript> tag, default to cfscript comments
+        if (
+          // <cf tag exists, and
+          textBeforeSelection.search(/\<cf/) !== -1 &&
+          // selection contains "<cfscript or </cfscript", or
+          (fullSelectionText.search(/<\/?cfscript/) !== -1 ||
+            // the last instance of <cf or </cf is not <cfscript.
+            // (If the last instance is cfscript, it means we are within a cfscript tag
+            // and should use cfscript comments.)
+            textBeforeSelection
+              .match(/\<\/?cf(?!.*\<\/?cf).{0,6}/g)
+              .reverse()[0] !== '<cfscript')
+        ) {
+          languageConfig = Object.assign(languageConfig, {
+            comments: {
+              blockComment: ['<!---', '--->']
+            }
+          });
         }
-      } catch (e) {
-        console.log(e);
+        // set language config, which should enable the right kind of comments
+        vscode.languages.setLanguageConfiguration('lang-cfml', languageConfig);
+        vscode.commands.executeCommand('editor.action.commentLine');
       }
     }
   );
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-exports.deactivate = deactivate;
+exports.deactivate = function() {};
